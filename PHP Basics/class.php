77812dd6752022f120d1a8874cd8f693adc16d9c
naylin2 <nayylinhtoo@gmail.com>
2021-10-14 15:09:43 +0630

    added array test

--- PHP Basics/class.php
+++ PHP Basics/class.php
@@ -28,6 +28,22 @@ echo "<br>";
 $myPlant = new plant("Purple","Orchid");
 echo $myPlant -> output();
 
+$cars = array (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+);
+    
+for ($row = 0; $row < 4; $row++) {
+  echo "<p><b>Row number $row</b></p>";
+  echo "<ul>";
+  for ($col = 0; $col < 3; $col++) {
+    echo "<li>".$cars[$row][$col]."</li>";
+  }
+  echo "</ul>";
+}
+
 ?>
 
 </body>
