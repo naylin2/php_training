41168ee6aa9a2997fda8572fe21ec7fe70dda769
naylin2 <nayylinhtoo@gmail.com>
2021-10-17 20:10:10 +0630

    added db test

---
+++ PHP Basics/includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "yesdaddy";
+$db = "test";
+
+$conn = mysqli_connect($servername, $username, $password, $db) or die("Connection failed");
